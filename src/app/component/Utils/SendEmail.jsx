030fe96ba2670437db72184b61ff5dba0f24fa4b
PatrickEinstein <mohammedola1234@gmail.com>
2024-01-18 13:28:13 +0100

    f4

--- src/app/component/Utils/SendEmail.jsx
+++ src/app/component/Utils/SendEmail.jsx
@@ -1,27 +1,27 @@
-import nodemailer from 'nodemailer';
-export const Emailer = async (email, subject, message) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "patoctave99@gmail.com",
-        pass: "kjksxvvyxtoldilv",
-      },
-    });
+// import nodemailer from 'nodemailer';
+// export const Emailer = async (email, subject, message) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: "patoctave99@gmail.com",
+//         pass: "kjksxvvyxtoldilv",
+//       },
+//     });
 
-    const mailOptions = {
-      from: email,
-      to: "mohammedola1234@gmail.com",
-      subject: subject,
-      text: message,
-      // html: html,
-      // attachments: attachments,
-    };
+//     const mailOptions = {
+//       from: email,
+//       to: "mohammedola1234@gmail.com",
+//       subject: subject,
+//       text: message,
+//       // html: html,
+//       // attachments: attachments,
+//     };
 
-    await transporter.sendMail(mailOptions);
-    return "Mail sent successfully";
-  } catch (error) {
-    console.error(error.message);
-    return error.message;
-  }
-};
+//     await transporter.sendMail(mailOptions);
+//     return "Mail sent successfully";
+//   } catch (error) {
+//     console.error(error.message);
+//     return error.message;
+//   }
+// };
